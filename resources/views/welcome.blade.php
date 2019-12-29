69f39033dd57bd1bff733c4fc326216c635301fa
Ivano <ivanocisternino@gmail.com>
2019-12-29 15:58:27 +0100

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Portale del dipendente
                 </div>
 
                 <div class="links">
